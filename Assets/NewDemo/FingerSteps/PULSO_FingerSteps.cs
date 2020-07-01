62e4c907ec4cb34afe557022b747937563be73a2
ARPavel <artman31@gmail.com>
2020-07-01 09:11:00 +0300

    upd

--- Assets/NewDemo/FingerSteps/PULSO_FingerSteps.cs
+++ Assets/NewDemo/FingerSteps/PULSO_FingerSteps.cs
@@ -1,5 +1,4 @@
-﻿using extOSC.Components.Informers;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Data.SqlTypes;
 using TMPro.EditorUtilities;
